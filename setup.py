6a8d22b83d0ae2d514efbf14651ae1833ec5d90a
Thomas Grenfell Smith <thomathom@gmail.com>
2018-09-06 13:08:48 -0400

    Fixup long_description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='requests-jwt',
-    version='0.5',
+    version='0.5.1',
     url='https://github.com/tgs/requests-jwt',
     modules=['requests_jwt'],
     install_requires=[ 'requests', 'PyJWT' ],
@@ -15,6 +15,7 @@ setup(
     author='Thomas Grenfell Smith',
     author_email='thomathom@gmail.com',
     description='This package allows for HTTP JSON Web Token (JWT) authentication using the requests library.',
+    long_description=open('README.rst').read(),
     license='ISC',
     classifiers=[
         'Development Status :: 4 - Beta',
